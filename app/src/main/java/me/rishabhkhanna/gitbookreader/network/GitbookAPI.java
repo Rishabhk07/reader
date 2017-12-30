a24d3b7227466a69f76b2b07ecf3d9b66ce59afb
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-12-30 11:46:30 -0500

    non API path
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/me/rishabhkhanna/gitbookreader/network/GitbookAPI.java
+++ app/src/main/java/me/rishabhkhanna/gitbookreader/network/GitbookAPI.java
@@ -1,8 +1,29 @@
 package me.rishabhkhanna.gitbookreader.network;
 
+import android.content.Context;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by rishabhkhanna on 30/12/17.
  */
 
 public class GitbookAPI {
+    public static GitbookAPI instance;
+    public Retrofit retrofit;
+
+    public GitbookAPI(Context context) {
+        this.retrofit = new Retrofit.Builder()
+                .baseUrl("https://www.gitbook.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static GitbookAPI getInstance(Context context){
+        if(instance == null){
+            instance = new GitbookAPI(context);
+        }
+        return instance;
+    }
 }

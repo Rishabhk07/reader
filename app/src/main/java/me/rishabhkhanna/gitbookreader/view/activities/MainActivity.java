d52356839d877850b2d67d8bc1cb2662f31e0801
Rishabh Khanna <rishabhkhanna777@gmail.com>
2018-01-02 01:17:11 -0500

    bottom navigation menu inflate
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

---
+++ app/src/main/java/me/rishabhkhanna/gitbookreader/view/activities/MainActivity.java
@@ -0,0 +1,45 @@
+package me.rishabhkhanna.gitbookreader.view.activities;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import me.rishabhkhanna.gitbookreader.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    public static final String TAG = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.menu_explore:
+                        Log.d(TAG, "onNavigationItemSelected: Explore selected");
+                        break;
+                    case R.id.menu_topics:
+                        Log.d(TAG, "onNavigationItemSelected: topics selected");
+                        break;
+                    case R.id.menu_library:
+                        Log.d(TAG, "onNavigationItemSelected: library selected");
+                        break;
+                    case R.id.menu_author:
+                        Log.d(TAG, "onNavigationItemSelected: author selected");
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
+}

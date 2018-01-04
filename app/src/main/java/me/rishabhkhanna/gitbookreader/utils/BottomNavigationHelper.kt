a9b18bdc3f0b5c1229e2d73ee7617b9f345946d8
Rishabh Khanna <rishabhkhanna777@gmail.com>
2018-01-03 23:32:30 -0500

    add items to bottom navigaiot
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/me/rishabhkhanna/gitbookreader/utils/BottomNavigationHelper.kt
+++ app/src/main/java/me/rishabhkhanna/gitbookreader/utils/BottomNavigationHelper.kt
@@ -4,6 +4,9 @@ import android.support.design.internal.BottomNavigationItemView
 import android.support.design.internal.BottomNavigationMenuView
 import android.support.design.widget.BottomNavigationView
 import android.util.Log
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem
+import me.rishabhkhanna.gitbookreader.R
 import java.lang.reflect.Field
 
 /**
@@ -29,6 +32,20 @@ public class BottomNavigationHelper {
             } catch (e: IllegalAccessException) {
                 Log.d(TAG, "unable to change value of shift mode " + e.message)
             }
+        }
+
+        fun getBottomNavigationMenu(bottomNavigationView: AHBottomNavigation){
+
+// items on navigaiton bottom bar
+            val explore:AHBottomNavigationItem = AHBottomNavigationItem("Explore", R.drawable.ic_telescope);
+            val topic:AHBottomNavigationItem = AHBottomNavigationItem("Topics", R.drawable.ic_tag);
+            val library:AHBottomNavigationItem = AHBottomNavigationItem("Library", R.drawable.ic_book);
+            val author:AHBottomNavigationItem = AHBottomNavigationItem("Author", R.drawable.ic_telescope);
+            bottomNavigationView.addItem(explore)
+            bottomNavigationView.addItem(topic)
+            bottomNavigationView.addItem(library)
+            bottomNavigationView.addItem(author)
+
         }
     }
 }

e16d10c59e869b094d0ebaa686bbcbb4f4c66a7f
Rishabh Khanna <rishabhkhanna777@gmail.com>
2018-01-11 13:06:27 -0500

    api interface
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

---
+++ app/src/main/java/me/rishabhkhanna/gitbookreader/network/interfaces/apiInterface.java
@@ -0,0 +1,27 @@
+package me.rishabhkhanna.gitbookreader.network.interfaces;
+
+import me.rishabhkhanna.gitbookreader.models.AuthorBooks;
+import me.rishabhkhanna.gitbookreader.models.BookContents;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by rishabhkhanna on 11/01/18.
+ */
+
+public interface apiInterface {
+
+    @GET("author/{authorName}/books")
+    Call<AuthorBooks> getBooksByAuthor(
+            @Path("authorName") String authorName
+    );
+
+    @GET("book/{authorName}/{bookName}/contents/{chapter}/{sub}")
+    Call<BookContents> getBookContents(
+            @Path("authorName") String authorName,
+            @Path("bookName") String bookName,
+            @Path("chapter") String chapter,
+            @Path("sub") String sub
+    );
+}
---
+++ app/src/main/java/me/rishabhkhanna/gitbookreader/network/interfaces/gitbookInterface.java
@@ -0,0 +1,8 @@
+package me.rishabhkhanna.gitbookreader.network.interfaces;
+
+/**
+ * Created by rishabhkhanna on 11/01/18.
+ */
+
+public interface gitbookInterface {
+}

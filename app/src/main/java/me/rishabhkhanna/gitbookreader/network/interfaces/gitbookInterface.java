059b3e59f30bd123cc525c21125769c409825244
Rishabh Khanna <rishabhkhanna777@gmail.com>
2018-01-11 13:22:49 -0500

    gitbook interface
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/me/rishabhkhanna/gitbookreader/network/interfaces/gitbookInterface.java
+++ app/src/main/java/me/rishabhkhanna/gitbookreader/network/interfaces/gitbookInterface.java
@@ -1,8 +1,30 @@
 package me.rishabhkhanna.gitbookreader.network.interfaces;
 
+import me.rishabhkhanna.gitbookreader.models.TopicExplore;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
 /**
  * Created by rishabhkhanna on 11/01/18.
  */
 
 public interface gitbookInterface {
+
+    @Headers("x-pjax: true")
+    @GET("explore")
+    Call<TopicExplore> getNoParamsExplore(
+            @Query("page") String page,
+            @Query("lang") String lang
+    );
+
+    @Headers("x-pjax: true")
+    @GET("explore/topic/{topic}")
+    Call<TopicExplore> getGitbookExplore(
+            @Query("page") String page,
+            @Query("lang") String lang
+    );
+
 }
